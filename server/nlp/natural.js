cf7cd6f91fda2295bedc08cd0633f7cebddf722e
NITHIN N <nithinn9980@gmail.com>
2023-10-25 14:03:28 +0530

    nlp updated

--- server/nlp/natural.js
+++ server/nlp/natural.js
@@ -1,12 +1,14 @@
 const natural = require('natural');
 
 const classifier = (text, callback) => {
+    //loading the trained model
     natural.BayesClassifier.load('./nlp/trained.json', null, (err, classifier) => {
+        // checking whether there is any err or not 
         if (err){
             console.log("fail to classify the text\n", err);
             callback(null)
         }else 
-            callback(classifier.classify(text))
+            callback(classifier.classify(text)) //returning the department to which complaint belongs
     })
 }
 
--- server/nlp/train/index.js
+++ server/nlp/train/index.js
@@ -3,13 +3,18 @@ const natural = require('natural');
 
 const classifier = new natural.BayesClassifier()
 
+//reading the test.csv file and storing the data in variable data
 const data = fs.readFileSync('test.csv', 'utf8').split("\n")
-const examples = data.map(data => data.split(","))
+//spliting the data into text and label and storing it in testData variable
+const testData = data.map(data => data.split(","))
 
-examples.forEach(([value, label]) => {
+//adding the testData to classifier
+testData.forEach(([value, label]) => {
     classifier.addDocument(value, label)
 })
 
+//training the model
 classifier.train()
 
+//saving the trained model in json formate
 classifier.save("trained.json", (err, classifier) => {})
